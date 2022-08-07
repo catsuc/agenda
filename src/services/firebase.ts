6e3b71997555e4fd1ff8779d098903818334ca81
Miguel Ângelo <miguelcg958@gmail.com>
2022-08-06 22:26:00 -0300

    fix: resolves wrong variables names

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,13 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
